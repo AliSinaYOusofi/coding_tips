872542683270e2c41c7111b361d2f1cf24158c24
HajiAli <senayousofiali@gmail.com>
2024-03-31 13:43:21 +0430

    changed: about page and author data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,12 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'DevInsight',
+  author: 'Ali Sina Yousofi',
+  headerTitle: 'DevInsight',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/AliSinaYOusofi/coding_tips',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
